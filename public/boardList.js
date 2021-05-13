f999ee34fac0417cd54ea4da2bd66067fd9f79fd
BrianH <brianhwang29@gmail.com>
2021-05-13 17:51:21 +0900

    change it to pure js

--- public/boardList.js
+++ public/boardList.js
@@ -1,8 +1,16 @@
 "use strict";
 
-$(function(){
-    $(".table tbody tr").click(function(){
-        const bno = $(this).children().eq(0).text();
-        location.href = `/boardDetail/bno/${bno}`;
-    })
-})
+// $(function(){
+//     $(".table tbody tr").click(function(){
+//         const bno = $(this).children().eq(0).text();
+//         location.href = `/boardDetail/bno/${bno}`;
+//     })
+// })
+
+const td = document.querySelector('tbody tr'); 
+const bno = td.firstElementChild.innerHTML;  
+
+td.addEventListener("click", function () {
+    location.href = `/boardDetail/bno/${bno}` 
+});
+
